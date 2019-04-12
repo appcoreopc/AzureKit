8324702d8f99cad52504d3cfb1997d85040a95d0
jeremy woo <kepungmoin@hotmail.com>
2019-04-12 20:18:05 +1200

    azure development kit.

---
+++ AzureKitStorage/BlobStorageProvider.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Microsoft.WindowsAzure.Storage;
+using System.Threading.Tasks;
+
+namespace AzureKitStorage
+{
+    public class BlobStorageProvider
+    {
+        private readonly CloudBlobClient account; 
+        
+        public BlobStorageProvider(CloudBlobClient account)
+        {
+            this.account = account;
+        }
+
+        private async Task<CloudBlobContainer> GetContainerAsync(string name)
+        { 
+            var container = this.account.GetContainerReference(name);  
+            await container.CreateIfNotExistsAsync(); 
+            return container;     
+        }
+
+        public async Task<Uri> UploadBlob(string containerName, string blobName, string filename) 
+        { 
+            var container = await this.GetContainerAsync(containerName);
+            var blobObj = container.GetBlockBlobReference(blobName);            
+            await blobObj.UploadFromFileAsync(filename);
+            return blobObj.Uri;
+        }
+    }
+}
