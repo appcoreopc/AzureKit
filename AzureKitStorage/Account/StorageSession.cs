f4e8bf5c49fae33c441aed4f539ac8ef1550da8e
JeremyWoo <jeremy.woo@c3posttrade.com>
2019-04-15 14:25:02 +1200

    updated storage session

--- AzureKitStorage/Account/StorageSession.cs
+++ AzureKitStorage/Account/StorageSession.cs
@@ -1,17 +1,36 @@
-using System;
-using Microsoft.WindowsAzure.Storage.Blob;
 using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
 using System.Threading.Tasks;
 
 namespace AzureKitStorage.Account
 {
-    public class StorageSession : IStorageSession {
-
-        private CloudStorageAccount storage; 
-        public CloudStorageAccount CreateStorageProvider(string connection)         
-        {       
-            return CloudStorageAccount.Parse(connection);      
+    public static class StorageSession
+    {
+        public static CloudStorageAccount CreateStorageProvider(string connection)
+        {
+            return CloudStorageAccount.Parse(connection);
         }
-      
+
+        public static async Task<CloudBlobContainer> CreateContainerAsync(this CloudStorageAccount cloudStorageAccount, string containerName)
+        {
+            var container = cloudStorageAccount.CreateCloudBlobClient().GetContainerReference(containerName);
+            await container.CreateIfNotExistsAsync();
+            return container;
+        }
+
+        public static ICloudBlob CreatePageStorage(this CloudBlobContainer container, string blobName)
+        {
+            return container.GetPageBlobReference(blobName);
+        }
+
+        public static ICloudBlob CreateBlockStorage(this CloudBlobContainer container, string blobName)
+        {
+            return container.GetBlockBlobReference(blobName);
+        }
+
+        public static ICloudBlob CreateAppendStorage(this CloudBlobContainer container, string blobName)
+        {
+            return container.GetAppendBlobReference(blobName);
+        }
     }
 }
