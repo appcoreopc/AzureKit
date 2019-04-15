04ab8bd11cdf79a1b7d125d518d41b2636ce06fd
jeremy <jeremy.woo@c3posttrade.com>
2019-04-16 08:01:47 +1200

    added storage provider.

---
+++ AzureKitStorage/Provider/ContainerProvider.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.WindowsAzure.Storage.Blob;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AzureStorageKit.Provider
+{
+    class ContainerProvider
+    {
+        private readonly CloudBlobClient account;
+
+        public ContainerProvider(CloudBlobClient account)
+        {
+            this.account = account;
+        }
+
+        private async Task<CloudBlobContainer> CreateContainerAsync(string name)
+        {
+            var container = this.account.GetContainerReference(name);
+            await container.CreateIfNotExistsAsync();
+            return container;
+        }
+
+        public async Task<IList<Uri>> ListFiles(string containerName, string blobName, string filename)
+        {
+            var fileListing = new List<Uri>();
+            var token = new BlobContinuationToken();
+
+            do
+            {
+                var fileSegment = await this.account.ListBlobsSegmentedAsync(filename, token);
+
+                foreach (var item in fileSegment.Results)
+                {
+                    fileListing.Add(item.Uri);
+                }
+
+            } while (token != null);
+
+            return fileListing;
+        }
+    }
+}
---
+++ AzureKitStorage/Provider/IStorageProviderFeature.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AzureStorageKit.BlobProvider
+{
+    public interface IStorageProviderFeature
+    {
+
+        Task<Uri> UploadFile(string containerName, string blobName, string filename);
+
+        Task<IList<string>> ListFiles(string containerName, string blobName, string pattern);
+
+        Task<Uri> DonwloadFile(string containerName, string blobName, string filename);
+
+        Task<Uri> DeletedFile(string containerName, string blobName, string filename);
+
+    }
+}
